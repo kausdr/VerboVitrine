424eff6ccaa99e32a4a9f3e1c91001152bb4207f
Kauane <86927114+kausdr@users.noreply.github.com>
2023-11-07 10:45:22 -0300

    Projeto no Xcode criado

---
+++ VerboVitrine/VerboVitrine/VerboVitrineApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerboVitrineApp.swift
+//  VerboVitrine
+//
+//  Created by Kauane Santana on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct VerboVitrineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
