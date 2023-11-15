946963c4f6699dcab1d7a9a25de90c528231ff89
Kauane <86927114+kausdr@users.noreply.github.com>
2023-11-15 17:44:29 -0300

    block key

--- VerboVitrine/VerboVitrine/Models/Constants.swift
+++ VerboVitrine/VerboVitrine/Models/Constants.swift
@@ -11,5 +11,5 @@ enum Constants {
 //    static let openAIApiKey = "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" // ERRADA
 //    static let openAIApiKey = "sk-OdbsIUepgH0bitcowryhT3BlbkFJpCvlpPPPFhJIHStDpUv5p" //tirar o " --x " para a chave funcionar
     
-    static let openAIApiKey = "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"
+    static let openAIApiKey = "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--x"
 }
